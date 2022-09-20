e5525a6ccb88782f1f3b1a47cd700d6dc3bd73c1
Shaikhnbake <shaikhnajm21@gmail.com>
2022-09-19 21:47:34 -0400

    updated profile and top movies view
    Fixed remove from topmovies button

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -10,84 +10,84 @@ import './profile-view.scss';
 import { useEffect, useState } from "react";
 
 export function ProfileView(props) {
-    const [ user, setUser ] = useState(props.user);
+    const [user, setUser] = useState(props.user);
     /* const [ movies, setMovies ] = useState(props.movies); */
-    const [ topMovies, setTopMovies ] = useState([]);
-    
+    const [topMovies, setTopMovies] = useState([]);
+
     const token = localStorage.getItem('token');
     const localUser = localStorage.getItem('user');
 
     const topMoviesList = props.movies.filter((movies) => {
         return user.topMovies?.includes(movies._id);
     });
-  
+
     const getUser = () => {
         axios.get(`https://myflix1najm.herokuapp.com/users/${localUser}`, {
-          headers: { Authorization: `Bearer ${token}`}
+            headers: { Authorization: `Bearer ${token}` }
         })
-        .then(response => {
-            setUser(response.data);
-            setTopMovies(response.data.topMovies)
-        })
-        .catch(function (error) {
-          console.log(error);
-        })
-      }
-    
+            .then(response => {
+                setUser(response.data);
+                setTopMovies(response.data.topMovies)
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
     useEffect(() => {
         getUser();
     }, [])
 
-    
+
 
     const deleteUser = () => {
-      axios.delete(`https://myflix1najm.herokuapp.com/users/${localUser}`, {
-        headers: { Authorization: `Bearer ${token}`}
-      })
-      .then(response => {
-        localStorage.clear();
-        window.open('/','_self');
-        console.log('Account has been deleted!');
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
+        axios.delete(`https://myflix1najm.herokuapp.com/users/${localUser}`, {
+            headers: { Authorization: `Bearer ${token}` }
+        })
+            .then(response => {
+                localStorage.clear();
+                window.open('/', '_self');
+                console.log('Account has been deleted!');
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
     }
 
     return (
-    <Container>
-        <Row>
-            <Col xs={12} sm ={4}>
-                <Card>
-                    <Card.Body>
-                        <UserInfo name={user.username} email={user.email} birthday={user.birthday}/>
-                    </Card.Body>
-                </Card>
-            </Col>
-            <Col xs={12} sm ={8}>
-                <Card>
-                    <Card.Body>
-                        <UpdateUser user={user} />                    
-                    </Card.Body>
-                </Card>     
-            </Col>      
-        </Row>
+        <Container>
+            <Row>
+                <Col xs={12} sm={4}>
+                    <Card>
+                        <Card.Body>
+                            <UserInfo name={user.username} email={user.email} birthday={user.birthday} />
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col xs={12} sm={8}>
+                    <Card>
+                        <Card.Body>
+                            <UpdateUser user={user} />
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
 
-        <Row className="topMovies">
-            <Col>
-                <TopMovies topMoviesList={topMoviesList} />
-            </Col>
+            <Row className="topMovies">
+                <Col>
+                    <TopMovies topMoviesList={topMoviesList} token={token} localUser={localUser} />
+                </Col>
 
-        </Row>
-        <Row className="mt-5">
-           
-        </Row>
-        <Row>
-            <p>Do you wish to delete you account?</p>
+            </Row>
+            <Row className="mt-5">
+
+            </Row>
+            <Row>
+                <p>Do you wish to delete you account?</p>
                 <Button variant="danger" type="submit" onClick={deleteUser}>
                     <h1>CONFIRM DELETE</h1>
                 </Button>
-        </Row>
-     </Container>
+            </Row>
+        </Container>
     );
-  }
+}
--- src/components/profile-view/top-movies.jsx
+++ src/components/profile-view/top-movies.jsx
@@ -7,56 +7,56 @@ import './profile-view.scss'
 
 export function TopMovies(props) {
 
-    const { movie, topMoviesList, localUser, token} = props;
+    const { topMoviesList, localUser, token } = props;
 
 
     const removeTopMovie = (movieId) => {
         axios.delete(`https://myflix1najm.herokuapp.com/users/${localUser}/movies/${movieId}`, {
-        headers: { Authorization: `Bearer ${token}`}
-      })
-      .then(response => {
-        console.log('Movie has been removed from top movies.');
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
+            headers: { Authorization: `Bearer ${token}` }
+        })
+            .then(response => {
+                console.log('Movie has been removed from top movies.');
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
 
     }
 
 
     return (
-    <Container>
-        <Row>
-            <Col xs={12}>
-                <h1>Top Movies List</h1>
-            </Col>
-        </Row>
-        <Row>
-            {topMoviesList.map((movie) => {
-                return (
-                    <Container key={movie._id}>
-                        <Col className="topMovies-list" xs={12} md={6} lg={3} >
-                        <Card className="movie-card sm" style={{width: '18rem'}}>
-                            <Card.Img crossOrigin= "true" variant="top" src={movie.imgURL} />
-                            <Card.Body>
-                            <Card.Title>{movie.title}</Card.Title>
-                            <Card.Text>{movie.description}</Card.Text>
-                            <Link to={`/movies/${movie._id}`}>
-                                <Button variant="link">Read More..</Button>
-                            </Link>
-                            </Card.Body>
-                            <Card.Footer>
-                                <Button variant="secondary" onClick={() => removeTopMovie(movie._id) }>
-                                    Remove from Top Movies
-                                </Button>
-                            </Card.Footer>
-                        </Card>
-                        </Col>
+        <Container>
+            <Row>
+                <Col xs={12}>
+                    <h1>Top Movies List</h1>
+                </Col>
+            </Row>
+            <Row>
+                {topMoviesList.map((movie) => {
+                    return (
+                        <Container key={movie._id}>
+                            <Col className="topMovies-list" xs={12} md={6} lg={3} >
+                                <Card className="movie-card sm" style={{ width: '18rem' }}>
+                                    <Card.Img crossOrigin="true" variant="top" src={movie.imgURL} />
+                                    <Card.Body>
+                                        <Card.Title>{movie.title}</Card.Title>
+                                        <Card.Text>{movie.description}</Card.Text>
+                                        <Link to={`/movies/${movie._id}`}>
+                                            <Button variant="link">Read More..</Button>
+                                        </Link>
+                                    </Card.Body>
+                                    <Card.Footer>
+                                        <Button variant="secondary" onClick={() => removeTopMovie(movie._id)}>
+                                            Remove from Top Movies
+                                        </Button>
+                                    </Card.Footer>
+                                </Card>
+                            </Col>
                         </Container>
-                        )
-            })}
-        </Row>
-    </Container>
+                    )
+                })}
+            </Row>
+        </Container>
     )
 }
 
